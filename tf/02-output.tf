a6cf2e338e6939dab782fb9edcc965ecb055b6cb
brezginaor <128047004+brezginaor@users.noreply.github.com>
2025-05-14 16:02:52 +0300

    Update 02-output.tf

--- tf/02-output.tf
+++ tf/02-output.tf
@@ -1,5 +1,3 @@
-output "servers" {
-  value = {
-    serverip = openstack_compute_instance_v2.brezgina_server.access_ip_v4
-  }
-} 
+output "serverip" {
+  value = openstack_compute_instance_v2.brezgina_server.network[0].fixed_ip_v4
+}
